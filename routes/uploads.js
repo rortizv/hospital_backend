cd8e4eb68b1d811344ded21038a49cb818947fcf
Rafael Ortiz <rafael007@gmail.com>
2022-10-17 23:05:16 -0500

    Respond with default image when filename parameter is incorrect

--- routes/uploads.js
+++ routes/uploads.js
@@ -7,7 +7,7 @@ const { Router, application } = require('express');
 const expressFileUpload = require('express-fileupload');
 
 const { validarJWT } = require('../middlewares/validar-jwt');
-const { fileUpload } = require('../controllers/uploads');
+const { fileUpload, retornaImagen } = require('../controllers/uploads');
 
 
 const router = Router();
@@ -15,5 +15,6 @@ const router = Router();
 router.use(expressFileUpload());
 
 router.put('/:tipo/:id', validarJWT, fileUpload);
+router.get('/:tipo/:foto', retornaImagen);
 
 module.exports = router;
